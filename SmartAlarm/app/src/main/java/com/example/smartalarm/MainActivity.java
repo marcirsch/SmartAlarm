52edf6e190fb66807604026941ec63b74ed218c6
Marcell Rausch <marcell.rausch@gmail.com>
2019-10-29 14:47:29 +0100

    Set time every 1 second. Not yet tested

--- SmartAlarm/app/src/main/java/com/example/smartalarm/MainActivity.java
+++ SmartAlarm/app/src/main/java/com/example/smartalarm/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.smartalarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timeTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timeTV = findViewById(R.id.time);
+        startClockThread();
+    }
+
+    private void startClockThread() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+                String currentTime = sdf.format(new Date());
+                timeTV.setText(currentTime);
+            }
+        }, 0, 1000);
+
     }
 }
